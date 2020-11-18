440491dcd264ab5135fb3a7236f1b4465e24361a
SeiyaIwabuchi <0123ook@gmail.com>
2020-11-18 17:57:13 +0900

    セッションを操作ごとに生成、切断するようにした。

--- back/app/model/DAO.py
+++ back/app/model/DAO.py
@@ -8,34 +8,37 @@ from sqlalchemy.types import Integer, String, DateTime
 import datetime
 from sqlalchemy.orm import sessionmaker
 from.MemoData import MemoData
+import inspect
 
 engine=create_engine("mysql://docker:docker@192.168.1.124:3306/test_database?charset=utf8")
 Base=declarative_base()
 Base.metadata.create_all(bind=engine, checkfirst=False)
 SessionClass=sessionmaker(engine) #セッションを作るクラスを作成
-session = SessionClass()
-print(session.__class__)
-class MySessionClass(session.__class__):
+
+class MySessionClass():
+    def __init__(self,session):
+        self.session = session
+
     def __enter__(self):
         Debug.log("Connect to Database.")
-        return self
+        return self.session
     
     def __exit__(self, ex_type, ex_value, trace):
-        self.close_all()
+        self.session.close()
         Debug.log("Close Database session.")
 
-with MySessionClass(engine) as session:
+with MySessionClass(SessionClass()) as session:
     Debug.log("Connected to Database.")
 
 def clearAll():
     #データクリア
-    with MySessionClass(engine).Session() as session:
+    with MySessionClass(SessionClass()) as session:
         for r in session.Session.query(MemoData).all():
             session.delete(r)
         session.commit()
 
 def insertOne(memoData:MemoData):
-    with MySessionClass(engine) as session:
+    with MySessionClass(SessionClass()) as session:
         session.add(memoData)
         try:
             session.commit()
@@ -43,22 +46,20 @@ def insertOne(memoData:MemoData):
             pass
 
 def getAll():
-    with MySessionClass(engine) as session:
+    with MySessionClass(SessionClass()) as session:
         data = session.query(MemoData).all()
     return data
 
 def deleteOne(memoData:MemoData):
-    with MySessionClass(engine) as session:
+    with MySessionClass(SessionClass()) as session:
         session.delete(memoData)
         session.commit()
 
 def updateOne(memoData:MemoData):
-    with MySessionClass(engine) as session:
+    with MySessionClass(SessionClass()) as session:
         oldData:MemoData = session.query(MemoData).filter(MemoData.id==memoData.id).first()
         Debug.log(oldData.toDict())
         Debug.log(memoData.toDict())
         oldData.title = memoData.title
         oldData.body = memoData.body
         session.commit()
-
-getAll()
